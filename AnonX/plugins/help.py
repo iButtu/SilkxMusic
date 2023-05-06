c95ce84648498f9f167044553b85c25dcb398857
Buttu <129733301+iButtu@users.noreply.github.com>
2023-05-06 17:32:40 +0530

    Update help.py
    Signed-off-by: Buttu <129733301+iButtu@users.noreply.github.com>

--- AnonX/plugins/help.py
+++ AnonX/plugins/help.py
@@ -143,3 +143,27 @@ async def helper_cb(client, CallbackQuery, _):
         await CallbackQuery.edit_message_text(
             helpers.HELP_12, reply_markup=keyboard
         )
+    elif cb == "hb13":
+        await CallbackQuery.edit_message_text(
+            helpers.HELP_13, reply_markup=keyboard
+        )
+    elif cb == "hb14":
+        await CallbackQuery.edit_message_text(
+            helpers.HELP_14, reply_markup=keyboard
+        )
+    elif cb == "hb15":
+        await CallbackQuery.edit_message_text(
+            helpers.HELP_15, reply_markup=keyboard
+        )
+    elif cb == "hb16":
+        await CallbackQuery.edit_message_text(
+            helpers.HELP_16, reply_markup=keyboard
+        )
+    elif cb == "hb17":
+        await CallbackQuery.edit_message_text(
+            helpers.HELP_17, reply_markup=keyboard
+        )
+    elif cb == "hb18":
+        await CallbackQuery.edit_message_text(
+            helpers.HELP_18, reply_markup=keyboard
+        )    
